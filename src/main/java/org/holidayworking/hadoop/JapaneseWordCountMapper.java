780192a7c50fd0fe6de842c1fbd2239a0a38f299
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2012-02-28 23:41:35 +0900

    日本語のワードカウントを実装
    形態素解析エンジンとして Kuromoji を使ってみた。

---
+++ src/main/java/org/holidayworking/hadoop/JapaneseWordCountMapper.java
@@ -0,0 +1,25 @@
+package org.holidayworking.hadoop;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.atilika.kuromoji.Token;
+import org.atilika.kuromoji.Tokenizer;
+
+class JapaneseWordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        Tokenizer tokenizer = Tokenizer.builder().build();
+        String line = value.toString();
+        List<Token> result = tokenizer.tokenize(line);
+        for (Token token : result) {
+            context.write(new Text(token.getSurfaceForm()), new IntWritable(1));
+        }
+    }
+
+}
